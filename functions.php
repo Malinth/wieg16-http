c9e53fcbd225424f29a1189d83c91ca9c8f5bf79
Malinth <malin.thunberg@hotmail.com>
2017-11-11 13:09:21 +0100

    Update

--- functions.php
+++ functions.php
@@ -38,20 +38,7 @@
 
     // Uppgift 6
 
-    // function redirect ($url, $code = 302) {
-    // header('Location: https://google.se');
-    //     $url =>
-    //     header()
-    // }
-
-    // <?php
-    // if (!(isset($_SERVER['HTTPS']) && ($_SERVER['HTTPS'] == 'on' || 
-    //    $_SERVER['HTTPS'] == 1) ||  
-    //    isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&   
-    //    $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'))
-    // {
-    //    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    //    header('HTTP/1.1 301 Moved Permanently');
-    //    header('Location: ' . $redirect);
-    //    exit();
-    // }
+ function redirect ($url, $code = 302) {
+    header('Location:' . $url, true, $code);
+   }
+   redirect("http://www.google.com");
